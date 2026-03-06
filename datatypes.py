4956176ac4fc319c90c04a2a984043a32307c498
mssrinivasulu <mssrinivasulu@gmail.com>
2026-03-07 05:28:15 +0530

    please update

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+24534
+var = 8347
+print(var)
+print(type(var))
+var = str(var)
+print(var)
+print(type(var))
+var = float(var)
+print(var)
+print(type(var))
+var = bool(var)
+print(var)
+print(type(var))
